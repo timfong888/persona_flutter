0f06b84c84d71da0ea8514091f4209b924851019
Tim Fong <timfong888@gmail.com>
2022-06-29 08:13:23 -0700

    accepted platform/ through VSC

--- lib/src/platform/persona_method_channel.dart
+++ lib/src/platform/persona_method_channel.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/services.dart';
 
-<<<<<<< HEAD
-import '../types/attributes.dart';
 import '../types/configurations.dart';
-import '../types/relationships.dart';
-=======
-import '../types/configurations.dart';
->>>>>>> 9f48fbd924ecdfa76d945677fb1a5f5d359c9b57
 import 'persona_platform_interface.dart';
 
 class PersonaMethodChannel extends PersonaPlatformInterface {
@@ -23,26 +17,6 @@ class PersonaMethodChannel extends PersonaPlatformInterface {
     return _channel.invokeMethod('init', configuration.toJson());
   }
 
-<<<<<<< HEAD
-  Future<dynamic> _onMethodCall(MethodCall call) async {
-    switch (call.method) {
-      case 'onSuccess':
-        final attributes =
-            InquiryAttributes.fromJson(call.arguments['attributes']);
-        final relationships =
-            InquiryRelationships.fromJson(call.arguments['relationships']);
-        onSuccess?.call(
-            call.arguments['inquiryId'] as String, attributes, relationships);
-        break;
-
-      case 'onFailed':
-        final attributes =
-            InquiryAttributes.fromJson(call.arguments['attributes']);
-        final relationships =
-            InquiryRelationships.fromJson(call.arguments['relationships']);
-        onFailed?.call(
-            call.arguments['inquiryId'] as String, attributes, relationships);
-=======
   @override
   Future<void> start() async {
     return _channel.invokeMethod('start');
@@ -56,7 +30,6 @@ class PersonaMethodChannel extends PersonaPlatformInterface {
         Map<String, dynamic> fields = (call.arguments['fields'] as Map).map(
             (key, value) => MapEntry<String, dynamic>(key.toString(), value));
         onComplete?.call(inquiryId, status, fields);
->>>>>>> 9f48fbd924ecdfa76d945677fb1a5f5d359c9b57
         break;
       case "onCanceled":
         String? inquiryId = call.arguments['inquiryId'] as String?;
--- lib/src/platform/persona_platform_interface.dart
+++ lib/src/platform/persona_platform_interface.dart
@@ -1,11 +1,7 @@
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 import '../types/configurations.dart';
-<<<<<<< HEAD
-import '../types/typedefs.dart';
-=======
 import '../types/enums.dart';
->>>>>>> 9f48fbd924ecdfa76d945677fb1a5f5d359c9b57
 import 'persona_method_channel.dart';
 
 abstract class PersonaPlatformInterface extends PlatformInterface {
@@ -26,13 +22,8 @@ abstract class PersonaPlatformInterface extends PlatformInterface {
     _instance = instance;
   }
 
-<<<<<<< HEAD
-  /// Called on a successful inquiry.
-  InquirySuccessCallback? onSuccess;
-=======
   /// The inquiry completed
   InquiryCompleteCallback? onComplete;
->>>>>>> 9f48fbd924ecdfa76d945677fb1a5f5d359c9b57
 
   /// The inquiry was cancelled by the user
   InquiryCanceledCallback? onCanceled;
@@ -40,15 +31,11 @@ abstract class PersonaPlatformInterface extends PlatformInterface {
   /// The inquiry errored
   InquiryErrorCallback? onError;
 
-<<<<<<< HEAD
-  Future<void> start({required InquiryConfiguration configuration}) async {
-=======
   Future<void> init({required InquiryConfiguration configuration}) async {
     throw UnimplementedError('init() has not been implemented.');
   }
 
   Future<void> start() async {
->>>>>>> 9f48fbd924ecdfa76d945677fb1a5f5d359c9b57
     throw UnimplementedError('start() has not been implemented.');
   }
 }
